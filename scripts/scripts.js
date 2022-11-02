54ca14edb656919bc03bc9d6e5c4e64515e4e98c
C-Macargo <caio.camargos97@gmail.com>
2022-11-02 18:37:30 -0300

    Layout + Strcture (Home page) V1 + Rendering all quizz

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,23 +9,23 @@ PromisseGetQuizz.then(ValidAllQuizzesResponse)
 function ValidAllQuizzesResponse(response){
     AllQuizzes = response.data;
     console.log(AllQuizzes)
+    RenderAllQUizzes()
 }
 
 function RenderAllQUizzes(){
-    const AllQuizzList = document.querySelector(".all-quizzes")
-    AllQuizzList.innerHTML = "";
+    const AllQuizzList = document.querySelector(".premade_quizzes")
 
+    for (let i = 0; i < AllQuizzes.length; i++ ){
+        let ChosenQuizz = AllQuizzes[i]
+        AllQuizzList.innerHTML += 
+        `   
 
-    for (let i = 0; i > AllQuizzes.length; i++ ){
+        <div class="quizz_box">
+            <img src="${ChosenQuizz.image}" class="quizz-pic">
+            <span> ${ChosenQuizz.title}</span>                    
+        </div>
 
-
-
-        
+        `
     }
-
-
-
-
-
-
 }
+
