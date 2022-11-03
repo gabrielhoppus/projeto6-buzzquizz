231e84d837ebabec1060e44bb0fd8347cad49128
C-Macargo <caio.camargos97@gmail.com>
2022-11-03 16:48:54 -0300

    Answer Quiz v0.3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,9 @@ let CreatedQuizzHelp = {};  /*Variável para adcionar ao objeto para enviar o qu
 
 
 
+let QuizzQuestionCount = ""; /*Variável para contar numero de questoes ao criar o quizz*/
+let QuizzLevels = ""; /*Variável para contar numero de niveis ao criar o quizz*/
+
 
 
 /*Código para buscar todos os Quizzes*/
@@ -44,17 +47,63 @@ function StartQuizz(){
 
     const QuizzTitle = document.querySelector(".quizz_title").value
     const QuizzImageUrl = document.querySelector(".quizz_url").value
-    const QuizzQuestionCount = document.querySelector(".quizz_questions").value
-    const QuizzLevels = document.querySelector(".quizz_level").value
+    QuizzQuestionCount = document.querySelector(".quizz_questions").value
+    QuizzLevels = document.querySelector(".quizz_level").value
     
         CreatedQuizz = { 
         title: QuizzTitle,
         image: QuizzImageUrl,
     }
-    console.log(CreatedQuizz)
         /*Código para esconder a primeira tela e mostrar a segunda*/
         const CurrentPage = document.querySelector(".first_screen");
         CurrentPage.classList.add("hidden")
         const NextPage = document.querySelector(".second_screen")
         NextPage.classList.remove("hidden")
-}   /*teste*/
+        CreateNumberOfQuestions()
+}   
+
+
+function CreateNumberOfQuestions(){
+    const SecondPageList = document.querySelector(".second_screen")
+
+    for(let i = 0 ; i < QuizzQuestionCount; i++ ){
+
+        SecondPageList.innerHTML +=
+        `  
+        <div class="input_container_question" onclick="SecondPageUnwrapContainer(this)">
+        <div class="hidden_container">
+            <span>Pergunta ${i+1}</span>
+            <img class="expand_questions" src="../assets/expand.png" >
+        </div>
+        <div class="wrap_container hidden " >
+            <div class="question_container">
+                <input class="quizz_input quizz_question " type="text" placeholder="Texto da pergunta">
+                <input class="quizz_input quizz_background " type="text" placeholder="Cor de fundo da pergunta">
+            </div>
+            <span>Resposta correta</span>
+            <input class="quizz_input quizz_correct " type="text" placeholder="Resposta correta">
+            <input class="quizz_input quizz_img" type="text" placeholder="URL da imagem">
+            <span>Respostas incorretas</span>
+            <input class="quizz_input quizz_incorrect first" type="text" placeholder="Resposta incorreta 1">
+            <input class="quizz_input quizz_img" type="text" placeholder="URL da imagem 1">
+            <input class="quizz_input quizz_incorrect second" type="text" placeholder="Resposta incorreta 2">
+            <input class="quizz_input quizz_img" type="text" placeholder="URL da imagem 2">
+            <input class="quizz_input quizz_incorrect third" type="text" placeholder="Resposta incorreta 3">
+            <input class="quizz_input quizz_img" type="text" placeholder="URL da imagem 3">   
+        </div>
+    </div>
+        `  
+    }
+
+    
+}
+
+
+
+
+
+function SecondPageUnwrapContainer(Unwraper){
+    
+    Unwraper.childNodes[3].classList.toggle('hidden')
+    console.log(UnwraperHelper)
+}   
