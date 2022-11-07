62db0f3c7ea9af0b35e65e22d94b957b2ac8e5b6
gabrielhoppus <luizgabriel.84@hotmail.com>
2022-11-07 18:14:02 -0300

    FINAL VERSION

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,28 +29,40 @@ function RenderAllQUizzes(){
     }
     const noQuiz = document.querySelector('.no_quizzes')
     const yesQuiz = document.querySelector('.yes_quizzes')
-    const userQuiz = document.querySelector('.created_quizzes')
+
     
     if (lista.length !== 0){
         noQuiz.classList.add('hidden')
         yesQuiz.classList.remove('hidden')
     }
 
-    userQuiz.innerHTML = ""
+    
 
     for (let i = 0; i < lista.length; i++ ){
-        //const promisseGetUserQuizz = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${lista[i]}`)
-        //promisseGetUserQuizz.then(userData)
-        userQuiz.innerHTML += `
+        const promisseGetUserQuizz = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${lista[i]}`)
+        promisseGetUserQuizz.then(userData)
+        /*userQuiz.innerHTML += `
             <div class="quizz_box">
-                <img src="./assets/Rectangle 36.png">
-                <span>O quão Potterhead é você?</span>                    
+                <img src="${userInfo.image}">
+                <span>${userInfo.title}</span>                    
             </div>
-        `
+        `*/
     }
-
 }
 
+function userData(response){
+    const userQuiz = document.querySelector('.created_quizzes')
+    userInfo = response.data
+    userQuiz.innerHTML = ""
+    for (let i = 0; i < lista.length; i++ ){
+        userQuiz.innerHTML += `
+        <div class="quizz_box">
+            <img src="${userInfo.image}">
+            <span>${userInfo.title}</span>                    
+        </div>
+    `
+    }
+}
 
 
 /*Função que manda o id para a url da página de resposta de quiz */
